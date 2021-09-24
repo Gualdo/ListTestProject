8d609ba016cda75cb23062d27ef1b6fc8b9a9cb6
Eduardo De La Cruz <edelacruz@teravisiontech.com>
2021-09-24 12:15:56 -0400

    Initial Commit

---
+++ ListTestProject/ListTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTestProjectApp.swift
+//  ListTestProject
+//
+//  Created by Eduardo David De La Cruz Marrero on 24/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ListTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
